003b9bebd249bfd412f0a0ce198e3652b8fb1fee
nicholas casey <ncasey@terpmail.umd.edu>
2023-04-10 15:33:01 -0400

    backend and react created

--- backend/app.py
+++ backend/app.py
@@ -7,9 +7,37 @@ app = Flask(__name__)
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="yourusername",
-  password="yourpassword"
+  user="root",
+  password="2255",
+  database="test"
 )
+
+mycursor = mydb.cursor()
+
+CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/create', methods=['POST', 'GET']) # type: ignore
+def create():
+  if request.method == 'POST' and request.json:
+      body = request.json
+      first_name = body['first_name']
+      last_name = body['last_name']
+      email = body['email']
+      phone = body['phone']
+      qry = "INSERT INTO users(first_name, last_name, email, phone) VALUES (%s, %s, %s, %s)"
+      vals = (first_name, last_name, email, phone)
+      mycursor.execute(qry, vals)
+      mydb.commit()
+      return jsonify({'status': 'success'})
+  if request.method == 'GET':
+      mycursor.execute("SELECT * FROM users")
+      data = mycursor.fetchall()
+      return jsonify(data)
+        
 
 if __name__ == '__main__':
     app.debug = True
